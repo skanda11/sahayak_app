1958ed3d72b31dcfe88d6cb57802560b0d26766b
Naveen kumar <naveen05p2137@gmail.com>
2025-07-27 04:36:04 +0000

    Many changes to the UI and backend.

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -9,11 +9,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyB7hNRT2GVb7yxluhNuT3xkcvmN4KPy06k",
+  authDomain: "sahayak-2311.firebaseapp.com",
+  projectId: "sahayak-2311",
+  storageBucket: "sahayak-2311.firebasestorage.app",
+  messagingSenderId: "1047129166081",
+  appId: "1:1047129166081:web:3a439fd8d82ba0c529cb00",
+  measurementId: "G-85L0GY4H98"
 };
